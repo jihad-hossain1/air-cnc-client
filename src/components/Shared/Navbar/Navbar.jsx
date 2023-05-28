751ebe336170d117e9a24c29665e2969f56f4125
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-28 11:03:04 +0600

    make a container for all page same size

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]"></div>
+    </div>
+  );
 };
 
 export default Navbar;
