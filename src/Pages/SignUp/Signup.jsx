000e447d8aa1a3878a20c66a97ee3377e2fbbd67
jihad-hossain1 <jihadkham934@gmail.com>
2023-05-31 12:41:29 +0600

    user create account with picture upload with imgbb

--- src/Pages/SignUp/Signup.jsx
+++ src/Pages/SignUp/Signup.jsx
@@ -1,114 +1,199 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+// import { Link } from "react-router-dom";
 
 import { FcGoogle } from "react-icons/fc";
-
+import { AuthContext } from "../../providers/AuthProvider";
+import { toast } from "react-hot-toast";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { FaSpinner } from "react-icons/fa";
 const Signup = () => {
-    return (
-      <>
-        <div className="flex justify-center items-center min-h-screen">
-          <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
-            <div className="mb-8 text-center">
-              <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
-              <p className="text-sm text-gray-400">Welcome to AirCNC</p>
-            </div>
-            <form
-              noValidate=""
-              action=""
-              className="space-y-6 ng-untouched ng-pristine ng-valid"
-            >
-              <div className="space-y-4">
-                <div>
-                  <label htmlFor="email" className="block mb-2 text-sm">
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Enter Your Name Here"
-                    className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
-                    data-temp-mail-org="0"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="image" className="block mb-2 text-sm">
-                    Select Image:
-                  </label>
-                  <input
-                    required
-                    type="file"
-                    id="image"
-                    name="image"
-                    accept="image/*"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="email" className="block mb-2 text-sm">
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    required
-                    placeholder="Enter Your Email Here"
-                    className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
-                    data-temp-mail-org="0"
-                  />
-                </div>
-                <div>
-                  <div className="flex justify-between">
-                    <label htmlFor="password" className="text-sm mb-2">
-                      Password
-                    </label>
-                  </div>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    required
-                    placeholder="*******"
-                    className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
-                  />
-                </div>
-              </div>
+      const navigate = useNavigate();
+        const location = useLocation();
+        const from = location.state?.from?.pathname || "/";
+  const {
+    loading,
+    setLoading,
+    createUser,
+    signInWithGoogle,
+    updateUserProfile,
+    } = useContext(AuthContext);
 
-              <div>
-                <button
-                  type="submit"
-                  className="bg-rose-500 w-full rounded-md py-3 text-white"
-                >
-                  Continue
-                </button>
-              </div>
-            </form>
-            <div className="flex items-center pt-4 space-x-1">
-              <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-              <p className="px-3 text-sm dark:text-gray-400">
-                Signup with social accounts
-              </p>
-              <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-            </div>
-            <div className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer">
-              <FcGoogle size={32} />
+    const handleCreateAccount = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const name = form.name.value;
+    
+    // upload picture
+    const image = form.image.files[0]
+    const formData = new FormData()
+    formData.append('image', image)
+    
+        const url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB}`;
+        
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        }).then(res => res.json()).then(data => {
+            console.log(data.data.display_url);
+            const imgUrl = data.data.display_url;
 
-              <p>Continue with Google</p>
-            </div>
-            <p className="px-6 text-sm text-center text-gray-400">
-              Already have an account?{" "}
-              <Link
-                to="/login"
-                className="hover:underline hover:text-rose-500 text-gray-600"
-              >
-                Login
-              </Link>
-              .
-            </p>
+            createUser(email, password)
+              .then(() => {
+                updateUserProfile(name, imgUrl)
+                    .then((result) => {
+                      navigate(from, { replace: true });
+                        // console.log("profile upload", result.user);
+                         toast.success("Successfully Account Created");
+                         setLoading(false);
+                  })
+                  .catch((error) => {
+                    setLoading(false);
+                    console.log(error.message);
+                    toast.error(`${error}`);
+                  });
+              })
+              .catch((error) => {
+                setLoading(false);
+                console.log(error.message);
+                toast.error(`${error}`);
+              });
+        })
+        console.log(url);
+        
+        
+     
+    };
+    const handleGoogleSignIn = () => {
+        console.log("clck");
+        signInWithGoogle()
+            .then((result) => {
+                console.log(result.user);
+                navigate(from, { replace: true });
+            })
+            .catch((error) => {
+                setLoading(false);
+                console.log(error.message);
+                toast.error(`${error}`);
+            });
+    }
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
+          <div className="mb-8 text-center">
+            <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
+            <p className="text-sm text-gray-400">Welcome to AirCNC</p>
           </div>
+          <form
+            onSubmit={handleCreateAccount}
+            noValidate=""
+            action=""
+            className="space-y-6 ng-untouched ng-pristine ng-valid"
+          >
+            <div className="space-y-4">
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Enter Your Name Here"
+                  className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                  data-temp-mail-org="0"
+                />
+              </div>
+              <div>
+                <label htmlFor="image" className="block mb-2 text-sm">
+                  Select Image:
+                </label>
+                <input
+                  required
+                  type="file"
+                  id="image"
+                  name="image"
+                  accept="image/*"
+                />
+              </div>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  required
+                  placeholder="Enter Your Email Here"
+                  className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                  data-temp-mail-org="0"
+                />
+              </div>
+              <div>
+                <div className="flex justify-between">
+                  <label htmlFor="password" className="text-sm mb-2">
+                    Password
+                  </label>
+                </div>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  required
+                  placeholder="*******"
+                  className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="bg-rose-500 w-full rounded-md py-3 text-white"
+              >
+                {loading ? (
+                  <FaSpinner
+                    className="m-auto animate-spin"
+                    size={24}
+                  ></FaSpinner>
+                ) : (
+                  "Continue"
+                )}
+              </button>
+            </div>
+          </form>
+          <div className="flex items-center pt-4 space-x-1">
+            <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+            <p className="px-3 text-sm dark:text-gray-400">
+              Signup with social accounts
+            </p>
+            <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+          </div>
+          <div
+            onClick={handleGoogleSignIn}
+            className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer"
+          >
+            <FcGoogle size={32} />
+
+            <p>Continue with Google</p>
+          </div>
+          <p className="px-6 text-sm text-center text-gray-400">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="hover:underline hover:text-rose-500 text-gray-600"
+            >
+              Login
+            </Link>
+            .
+          </p>
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Signup;
