76159878a63fae21d5ec9078be0efa80f9cbb36f
jihad-hossain1 <jihadkham934@gmail.com>
2023-06-01 05:01:39 +0600

    load data in room component with make a single card with group scale with a spinner when data is loading

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Categories from "../../../components/Category/Categories";
+import Rooms from "../../../components/Rooms/Rooms";
 // import Categories from "../../../components/Categories/Categories";
 
 const Home = () => {
   return (
     <div>
       <Categories></Categories>
+      <Rooms></Rooms>
     </div>
   );
 };
---
+++ src/components/Shared/Loader/Loader.jsx
@@ -0,0 +1,19 @@
+import { CircleLoader } from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div
+      className="
+      h-[70vh]
+      flex 
+      flex-col 
+      justify-center 
+      items-center 
+    "
+    >
+      <CircleLoader size={100} color="red" />
+    </div>
+  );
+};
+
+export default Loader;
